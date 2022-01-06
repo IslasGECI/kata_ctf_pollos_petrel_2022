9e725a8ac24e4aada13381c70bb223012ad7f6a1
nepito <braulio.rojas@islas.org.mx>
2022-01-06 12:44:43 -0700

    🚧 Split data

--- src/aed.R
+++ src/aed.R
@@ -4,3 +4,9 @@ data <- read_csv("pollos_petrel/train.csv")
 
 long_tarso <- data %>% 
   drop_na(Longitud_tarso)
+
+long_tarso %>% sample_frac(size = 0.8, replace = FALSE)
+
+
+id_train <- sample(nrow(long_tarso), floor(nrow(long_tarso)*.8))
+data_test <- subset(long_tarso, id %in% id_train)
