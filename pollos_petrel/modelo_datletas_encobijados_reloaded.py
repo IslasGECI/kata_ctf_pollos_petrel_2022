53df61f6c46d8c4d2d60be54948d733e3fe037d4
maritza <maritza.bello@islas.org.mx>
2022-01-06 14:18:35 -0800

    🚧 Mass interval 0.9 🙄 and mean

--- pollos_petrel/modelo_datletas_encobijados_reloaded.py
+++ pollos_petrel/modelo_datletas_encobijados_reloaded.py
@@ -13,7 +13,7 @@ for n_row, data_row in test_set.iterrows():
     if np.isnan(data_row["Longitud_ala"]):
         morphometric_variable = "Masa"
         mass_train = data_row[morphometric_variable]
-        interval = 0.8
+        interval = 0.9
     else:
         morphometric_variable = "Longitud_ala"
         mass_train = data_row[morphometric_variable]
