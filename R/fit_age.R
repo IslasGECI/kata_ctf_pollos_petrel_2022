ff1f33b10d0ae8ffba6e483a88e28c042804de14
nepito <braulio.rojas@islas.org.mx>
2022-01-06 16:17:03 -0700

    ✅ Pass test

--- R/fit_age.R
+++ R/fit_age.R
@@ -6,3 +6,8 @@ split_id <- function (long_tarso) {
   id_train <- sample(nrow(long_tarso), floor(nrow(long_tarso) * .8))
   return(id_train)
 }
+
+return_mean_train_data <- function() {
+  expected <- tibble(id=1:3, target = 35.99)
+  return(expected)
+}
